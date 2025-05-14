4abb98e9d843c2d4cb7b6935466f2630e94b87e5
Johannes Kerger <42kejo1bif@hft-stuttgart.de>
2025-05-14 15:52:46 +0200

    refactor(LoadManager): move methods around

--- src/LoadManager.java
+++ src/LoadManager.java
@@ -6,81 +6,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoadManager {
-    public static void load_anmeldungen(String path, Pruefung[] pruefungen) throws IOException {
-        Map<String, Set<String>> anmeldungen = new HashMap<>(); // Pruefung.qualifiedName -> MatrNr
-
-        for (Pruefung p : pruefungen) {
-            String qualified_name = p.getQualified_name();
-            // the following exception should never occur (-> internal logic error -> bug)
-            if (anmeldungen.containsKey(qualified_name))
-                throw new AssertionError("there are two pruefungen with the same name");
-            anmeldungen.put(qualified_name, new HashSet<>());
-        }
-
-        List<String> rows = Files.readAllLines(Paths.get(path));
-
-        if (rows.isEmpty())
-            throw new AssertionError();
-
-        List<String> rows_without_title = rows.subList(1, rows.size());
-        for (String row : rows_without_title) {
-            String[] columns = row.split(";");
-
-            //TODO: assert
-            String matr_nr = columns[0];
-            long pruefungs_nr = Long.parseLong(columns[5]);
-            String pruefungs_name = columns[6];
-            String stg = columns[2];
-            String pversion = columns[3];
-            String qualified_name = Pruefung.calculateQualifiedName(stg, pversion, pruefungs_nr, pruefungs_name);
-            // the following exception should never occur (-> internal logic error -> bug)
-            if (!anmeldungen.containsKey(qualified_name))
-                throw new AssertionError("a user anmeldung references a unknown pruefung: " + qualified_name);
-            anmeldungen.get(qualified_name).add(matr_nr);
-        }
-
-        for (Pruefung p : pruefungen) {
-            // the following exception should never occur (-> internal logic error -> bug)
-            if (p.getAnmeldungen() != null)
-                throw new AssertionError("The Anmeldungen of the Prüfung " + p + " was already loaded.");
-            p.setAnmeldungen(anmeldungen.get(p.getQualified_name()));
-        }
-    }
-
-    public static Pruefung[] load_missing_pruefungen_from_anmeldungen(String path, Pruefung[] pruefungen) throws IOException {
-        List<Pruefung> new_pruefungen = new LinkedList<>();
-        Map<String, Pruefung> pruefungen_by_qualified_name = new HashMap<>();
-
-        for (Pruefung p : pruefungen) {
-            // the following exception should never occur (-> internal logic error -> bug)
-            if (pruefungen_by_qualified_name.containsKey(p.getQualified_name()))
-                throw new AssertionError("there are two pruefungen with the same name");
-            pruefungen_by_qualified_name.put(p.getQualified_name(), p);
-        }
-
-        List<String> rows = Files.readAllLines(Paths.get(path));
-
-        if (rows.isEmpty())
-            throw new AssertionError();
-
-        List<String> rows_without_title = rows.subList(1, rows.size());
-        for (String row : rows_without_title) {
-            String[] columns = row.split(";");
-            long pruefungs_nr = Long.parseLong(columns[5]);
-            String name = columns[6];
-            String stg = columns[2];
-            String pversion = columns[3];
-            String qualified_name = Pruefung.calculateQualifiedName(stg, pversion, pruefungs_nr, name);
-            if (!pruefungen_by_qualified_name.containsKey(qualified_name)) {
-                Pruefung p = new Pruefung(pruefungs_nr, name, columns[2], columns[3], null, null);
-                new_pruefungen.add(p);
-                pruefungen_by_qualified_name.put(qualified_name, p);
-            }
-        }
-
-        return new_pruefungen.toArray(new Pruefung[0]);
-    }
-
     public static Pruefung[] load_pruefungen(String path) throws IOException {
         List<Pruefung> pruefungen = new LinkedList<>();
         Set<String> existingPruefungen = new HashSet<>(); // check existingPruefungen by qualifiedName
@@ -166,5 +91,80 @@ public class LoadManager {
             existingPruefungen.add(qualified_name);
         }
         return pruefungen.toArray(new Pruefung[0]);
+    }
+
+    public static Pruefung[] load_missing_pruefungen_from_anmeldungen(String path, Pruefung[] pruefungen) throws IOException {
+        List<Pruefung> new_pruefungen = new LinkedList<>();
+        Map<String, Pruefung> pruefungen_by_qualified_name = new HashMap<>();
+
+        for (Pruefung p : pruefungen) {
+            // the following exception should never occur (-> internal logic error -> bug)
+            if (pruefungen_by_qualified_name.containsKey(p.getQualified_name()))
+                throw new AssertionError("there are two pruefungen with the same name");
+            pruefungen_by_qualified_name.put(p.getQualified_name(), p);
+        }
+
+        List<String> rows = Files.readAllLines(Paths.get(path));
+
+        if (rows.isEmpty())
+            throw new AssertionError();
+
+        List<String> rows_without_title = rows.subList(1, rows.size());
+        for (String row : rows_without_title) {
+            String[] columns = row.split(";");
+            long pruefungs_nr = Long.parseLong(columns[5]);
+            String name = columns[6];
+            String stg = columns[2];
+            String pversion = columns[3];
+            String qualified_name = Pruefung.calculateQualifiedName(stg, pversion, pruefungs_nr, name);
+            if (!pruefungen_by_qualified_name.containsKey(qualified_name)) {
+                Pruefung p = new Pruefung(pruefungs_nr, name, columns[2], columns[3], null, null);
+                new_pruefungen.add(p);
+                pruefungen_by_qualified_name.put(qualified_name, p);
+            }
+        }
+
+        return new_pruefungen.toArray(new Pruefung[0]);
+    }
+
+    public static void load_anmeldungen(String path, Pruefung[] pruefungen) throws IOException {
+        Map<String, Set<String>> anmeldungen = new HashMap<>(); // Pruefung.qualifiedName -> MatrNr
+
+        for (Pruefung p : pruefungen) {
+            String qualified_name = p.getQualified_name();
+            // the following exception should never occur (-> internal logic error -> bug)
+            if (anmeldungen.containsKey(qualified_name))
+                throw new AssertionError("there are two pruefungen with the same name");
+            anmeldungen.put(qualified_name, new HashSet<>());
+        }
+
+        List<String> rows = Files.readAllLines(Paths.get(path));
+
+        if (rows.isEmpty())
+            throw new AssertionError();
+
+        List<String> rows_without_title = rows.subList(1, rows.size());
+        for (String row : rows_without_title) {
+            String[] columns = row.split(";");
+
+            //TODO: assert
+            String matr_nr = columns[0];
+            long pruefungs_nr = Long.parseLong(columns[5]);
+            String pruefungs_name = columns[6];
+            String stg = columns[2];
+            String pversion = columns[3];
+            String qualified_name = Pruefung.calculateQualifiedName(stg, pversion, pruefungs_nr, pruefungs_name);
+            // the following exception should never occur (-> internal logic error -> bug)
+            if (!anmeldungen.containsKey(qualified_name))
+                throw new AssertionError("a user anmeldung references a unknown pruefung: " + qualified_name);
+            anmeldungen.get(qualified_name).add(matr_nr);
+        }
+
+        for (Pruefung p : pruefungen) {
+            // the following exception should never occur (-> internal logic error -> bug)
+            if (p.getAnmeldungen() != null)
+                throw new AssertionError("The Anmeldungen of the Prüfung " + p + " was already loaded.");
+            p.setAnmeldungen(anmeldungen.get(p.getQualified_name()));
+        }
     }
 }
