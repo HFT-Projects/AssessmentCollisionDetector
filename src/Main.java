b0eddea41edfffd2ed77e35edaa3c44a9b520e79
Johannes Kerger <42kejo1bif@hft-stuttgart.de>
2025-06-20 15:23:15 +0200

    feat(Main): add check if javafx sdk is loaded properly.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import gui.ExamGUI;
 
 class Main {
     public static void main(String[] args) { //TODO: error handling
+        if (ModuleLayer.boot().modules().stream().map(Module::getName).noneMatch(s -> s.contains("javafx")))
+            throw new RuntimeException("javafx sdk not loaded properly. See README file.");
         ExamGUI.run();
     }
 }
