286a1ed6a1c5bf7cf5e83ad73f32d28876a3bce4
Johannes Kerger <42kejo1bif@hft-stuttgart.de>
2025-06-23 03:14:06 +0200

    doc(Main): add javafx notice

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import gui.MainGUI;
 
 class Main {
     public static void main(String[] args) {
+        // ATTENTION: the program would also START WITHOUT the javafx sdk included but this would lead to undefined behavior and bugs in the gui.
+        // therefore this chack assures that the javafx sdk was imported properly.
         if (ModuleLayer.boot().modules().stream().map(Module::getName).noneMatch(s -> s.contains("javafx")))
             throw new RuntimeException("javafx sdk not loaded properly. See README file.");
         MainGUI.run();
